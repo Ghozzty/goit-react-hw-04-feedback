81b3f455a9040ced20355d0853874ff449ecd1e5
Fedor <ghozztt@gmail.com>
2022-11-11 20:53:40 +0200

    cr rep hw-4-1

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,64 +1,63 @@
-import { Component } from "react";
-import { Statistics } from "components/statistics/statistics";
-import { FeedbackOptions } from "components/FeedbackOptions/FeedbackOptions";
-import { Section } from "components/section/section";
-import { Notification } from "components/notification/notification";
+import { Component } from 'react';
+import { Statistics } from 'components/statistics/statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Section } from 'components/section/section';
+import { Notification } from 'components/notification/notification';
 
-export class Feedback extends Component{
-
-state = {
+export class Feedback extends Component {
+  state = {
     good: 0,
     neutral: 0,
-    bad: 0
-    }
+    bad: 0,
+  };
 
-// clickEvent = (e) => this.setState((prevState)=> {
-//     const nameBtn = [e.target.textContent].join('').toLowerCase();
-    
-//     return {[nameBtn]: prevState[nameBtn] + 1}
-// });
+  // clickEvent = (e) => this.setState((prevState)=> {
+  //     const nameBtn = [e.target.textContent].join('').toLowerCase();
 
-clickEvent = (e) => this.setState((prevState)=> {
-    const nameBtn = e.target.name;
-       
-    return {[nameBtn]: prevState[nameBtn] + 1}
-});
+  //     return {[nameBtn]: prevState[nameBtn] + 1}
+  // });
 
+  clickEvent = e =>
+    this.setState(prevState => {
+      const nameBtn = e.target.name;
 
-countTotalFeedback = () => {return (this.state.good + this.state.neutral + this.state.bad)};
+      return { [nameBtn]: prevState[nameBtn] + 1 };
+    });
 
-countPositiveFeedbackPercentage = () => {
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
     const total = this.countTotalFeedback();
-    if (total === 0 ){
-        return 0;
+    if (total === 0) {
+      return 0;
     }
-    return Math.round((this.state.good / total) * 100) ;
-}
+    return Math.round((this.state.good / total) * 100);
+  };
 
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback___hw-04">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.clickEvent}
+          />
 
-render() {
-
-    return (  
-        <div>
-        <Section title="Please leave feedback">
-            <FeedbackOptions
-                options={this.state} onLeaveFeedback={this.clickEvent}/>
-
-            {this.countTotalFeedback() > 0 ?
-                <Statistics 
-                good={this.state.good}
-                neutral={this.state.neutral} 
-                bad={this.state.bad} 
-                total={this.countTotalFeedback()} 
-                positivePercentage={this.countPositiveFeedbackPercentage()}/>
-            :
-                <Notification message="There is no feedback"/>
-            }
-        
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
         </Section>
-    
-        </div>
-        
-    )
-    }
+      </div>
+    );
+  }
 }
